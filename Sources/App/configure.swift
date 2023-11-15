f10b897c016c803c1ac92287da270fbb6767e3f9
Pedro Moura <pedromoura@Pedros-MacBook-Pro.local>
2023-11-15 11:56:56 -0300

    Fixing environment

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,7 +3,7 @@ import Fluent
 import FluentPostgresDriver
 
 public func configure(_ app: Application) async throws {
-    try app.databases.use(.postgres(url: ProcessInfo.processInfo.environment["DB_CONNECTION_STRING"] ?? ""), as: .psql)
+    try app.databases.use(.postgres(url: Environment.get("DB_CONNECTION_STRING") ?? ""), as: .psql)
     
     app.migrations.add(CreateOrder())
 
